a85c1f15fcfd021c083798ba473c035031b1edfc
alexandremcosta <alexandremcost@gmail.com>
2013-09-06 20:02:08 -0300

    refactoring PlayerGroup: composition over inheritance

--- lib/pucker/player_group.rb
+++ lib/pucker/player_group.rb
@@ -1,23 +1,33 @@
+require 'forwardable'
 require_relative 'player'
 
 module Pucker
-  class PlayerGroup < Array
+  class PlayerGroup
+    include Enumerable
+
+    extend Forwardable
+    def_delegators :container, :each
+
+    attr_reader :container
+
+    def initialize(count=5)
+      count = 5 unless count.is_a? Integer
+      @container = Array.new(count) { player_source.call }
+    end
+
     def set_hands(dealer)
-      self.each do |player|
+      @container.each do |player|
         player.set_hand(dealer.deal, dealer.deal)
       end
     end
 
-    def create_players(num_of_players)
-      num_of_players.times do self << player_source.call end
+    def rotate_positions
+      @container.rotate!
     end
 
+    private
     def player_source
       @player_source ||= Player.public_method(:new)
-    end
-
-    def rotate_positions
-      self.rotate!
     end
   end
 end
--- spec/pucker/player_group_spec.rb
+++ spec/pucker/player_group_spec.rb
@@ -3,18 +3,9 @@ require "ostruct"
 
 module Pucker
   describe PlayerGroup do
-    describe "#create_players" do
-      it "should create players as resquested" do
-        group = PlayerGroup.new
-        group.create_players(5)
-        group.should have(5).items
-      end
-    end
-
     describe "#set_hands" do
       it "should give 2 cards to each player" do
         players = PlayerGroup.new
-        players.create_players(2)
         dealer = OpenStruct.new(deal: :card)
         players.each do |p|
           p.should_receive(:set_hand).with(:card, :card)
