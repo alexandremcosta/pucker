b57c550416ed42091dd33e2452a2706739461423
alexandremcosta <alexandremcost@gmail.com>
2013-09-07 14:25:08 -0300

    PlayerGroup: initializer receives stack amount, and rotate positions refactors

--- lib/pucker/player_group.rb
+++ lib/pucker/player_group.rb
@@ -6,13 +6,14 @@ module Pucker
     include Enumerable
 
     extend Forwardable
-    def_delegators :container, :each
+    def_delegators :container, :each, :[]
 
     attr_reader :container
 
-    def initialize(count=5)
+    def initialize(count=5, amount=400)
       count = 5 unless count.is_a? Integer
-      @container = Array.new(count) { player_source.call }
+      amount = 400 unless amount.is_a? Integer
+      @container = Array.new(count) { player_source.call(amount) }
     end
 
     def set_hands(dealer)
@@ -21,8 +22,12 @@ module Pucker
       end
     end
 
-    def rotate_positions
+    def rotate_positions!
       @container.rotate!
+    end
+
+    def rotate_positions(index=1)
+      @container.rotate(index)
     end
 
     private
--- spec/pucker/player_group_spec.rb
+++ spec/pucker/player_group_spec.rb
@@ -3,14 +3,31 @@ require "ostruct"
 
 module Pucker
   describe PlayerGroup do
+    let(:players) { PlayerGroup.new }
+
+    describe "#[]" do
+      it "should delegate to container" do
+        players[1].should == players.container[1]
+      end
+    end
+
     describe "#set_hands" do
       it "should give 2 cards to each player" do
-        players = PlayerGroup.new
         dealer = OpenStruct.new(deal: :card)
         players.each do |p|
           p.should_receive(:set_hand).with(:card, :card)
         end
         players.set_hands(dealer)
+      end
+    end
+
+    describe "#rotate_positions!" do
+      before do
+        @initial_container = Array.new(players.container)
+        players.rotate_positions!
+      end
+      it "should rotate players positions" do
+        players.container.should == @initial_container.rotate
       end
     end
   end
